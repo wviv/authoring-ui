e05b176b52010af1a04d6084f258e55f366aadee
Quyen Ly <quyen.ly@huntermacdonald.com>
2018-06-28 11:29:12 +0700

    MAINT-242 Update function

--- app/shared/feedback/feedback.js
+++ app/shared/feedback/feedback.js
@@ -153,14 +153,6 @@ angular.module('singleConceptAuthoringApp')
               }
             });
 
-          hotkeys.bindTo(scope)
-            .add({
-              combo: 'alt+q',
-              description: 'Close all concepts',
-              callback: function() {
-                closeAllConcepts();
-              }
-            });
           
           // get the user information to determine role
           // values: AUTHOR, REVIEWER
@@ -818,6 +810,19 @@ angular.module('singleConceptAuthoringApp')
               if(sorting) {                
                 scope.viewedConcepts = $filter('orderBy')(scope.viewedConcepts, sorting === 'asc' ? '+fsn' : '-fsn');
               }
+
+              // Re-bind shortcut
+              $timeout(function () {
+                hotkeys.bindTo(scope)
+                  .add({
+                    combo: 'alt+q',
+                    description: 'Close all concepts',
+                    callback: function() {
+                      closeAllConcepts();
+                    }
+                  });
+              }, 1000);
+             
 
               deferred.resolve(response);
 
