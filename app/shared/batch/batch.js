4aecc86527eccfad6dcc89b1b53315cd57c40886
Chris Swires <csw@ihtsdo.org>
2019-10-11 09:11:08 +0100

    MAINT-849 - fields wired to projects and users endpoints

--- app/shared/batch/batch.js
+++ app/shared/batch/batch.js
@@ -4,10 +4,46 @@ angular.module('singleConceptAuthoringApp')
   .controller('batchCtrl', function ($scope, $rootScope, $modalInstance, scaService, metadataService, $location) {
     
     $scope.projects = null;
+    $scope.users = null;
 
     function initialize() {
+      var users = [];
+
+      function getUsers(start, end) {
+        var expand =  'users[' + start + ':' + end + ']';
+        scaService.getUsers(expand).then(function (response) {
+          if (response.users.items.length > 0) {
+            angular.forEach(response.users.items, function (item) {
+              if (item.key !== $rootScope.accountDetails.login) {
+                var user = {};
+                user.avatarUrl = item.avatarUrls['16x16'];
+                user.displayName = item.displayName;
+                user.email = item.emailAddress;
+                user.username = item.key;
+                users.push(user);
+              }
+            });
+            $scope.users = users;
+          }
+
+          if (response.users.size > end) {
+            getUsers(start + 50, end + 50);
+          }
+        },
+        function (error) {});
+      }
+        
+      getUsers(0,50);
+    
       $scope.projects = metadataService.getProjects();
     }
+    
+    $scope.getAvailableUsers = function(exceptList) {
+        if ($scope.users.length === 0) {
+          return [];
+        }
+        return $scope.users;
+      };
     
     $scope.close = function () {
       $modalInstance.close();
